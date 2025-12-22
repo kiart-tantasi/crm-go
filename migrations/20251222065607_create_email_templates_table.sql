e3c7e8f34d4a99aec332be2ca898d59ea9dc0a57
kiart-tantasi <kiarttantasi@gmail.com>
2025-12-22 13:58:18 +0700

    add templates

---
+++ migrations/20251222065607_create_email_templates_table.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE templates (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    alias VARCHAR(255) NOT NULL,
+    template TEXT NOT NULL,
+    date_added DATETIME NOT NULL,
+    added_by INT NOT NULL,
+    date_modified DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    modified_by INT,
+    FOREIGN KEY (added_by) REFERENCES users(id),
+    FOREIGN KEY (modified_by) REFERENCES users(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS templates;
+-- +goose StatementEnd
