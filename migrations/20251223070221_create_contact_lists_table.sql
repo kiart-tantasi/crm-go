863d4c65015da1bafd12560ce8037467889c128e
kiart-tantasi <kiarttantasi@gmail.com>
2025-12-24 22:52:30 +0700

    add date_added and added_by into contact_list_members

--- migrations/20251223070221_create_contact_lists_table.sql
+++ migrations/20251223070221_create_contact_lists_table.sql
@@ -16,6 +16,8 @@ CREATE TABLE contact_lists (
 CREATE TABLE contact_list_members (
     contact_list_id INT NOT NULL,
     contact_id INT NOT NULL,
+    date_added DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    added_by INT NOT NULL,
     PRIMARY KEY (contact_list_id, contact_id),
     FOREIGN KEY (contact_list_id) REFERENCES contact_lists(id) ON DELETE CASCADE,
     FOREIGN KEY (contact_id) REFERENCES contacts(id) ON DELETE CASCADE
