ae50c32f819e0e1072db7881dbf8519133740f34
kiart-tantasi <kiarttantasi@gmail.com>
2025-12-30 13:57:04 +0700

    add id to mapping tables

--- migrations/20251224160401_create_contact_list_emails_table.sql
+++ migrations/20251224160401_create_contact_list_emails_table.sql
@@ -1,9 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE contact_list_emails (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     contact_list_id INT NOT NULL,
     email_id INT NOT NULL,
-    PRIMARY KEY (contact_list_id, email_id),
+    CONSTRAINT uq_contact_list_id_email_id UNIQUE (contact_list_id, email_id),
     FOREIGN KEY (contact_list_id) REFERENCES contact_lists(id) ON DELETE CASCADE,
     FOREIGN KEY (email_id) REFERENCES emails(id) ON DELETE CASCADE
 );
